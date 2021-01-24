822e7d3115932ea2f2439dad6ef8befc6f8b6b62
Koelbe <76868849+Koelbe@users.noreply.github.com>
2021-01-23 21:58:39 -0500

    Added the programs files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace SimplePythonIDE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.NewButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.RunButton = new System.Windows.Forms.Button();
+            this.TextEditorBox = new System.Windows.Forms.TextBox();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 12.46944F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.38387F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 10.02445F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.RunButton, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TextEditorBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.TitleLabel, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.06289F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.66457F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10.48218F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(818, 477);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.NewButton);
+            this.flowLayoutPanel1.Controls.Add(this.OpenButton);
+            this.flowLayoutPanel1.Controls.Add(this.SaveButton);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 50);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(96, 373);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // NewButton
+            // 
+            this.NewButton.Location = new System.Drawing.Point(3, 3);
+            this.NewButton.Name = "NewButton";
+            this.NewButton.Size = new System.Drawing.Size(75, 23);
+            this.NewButton.TabIndex = 0;
+            this.NewButton.Text = "New";
+            this.NewButton.UseVisualStyleBackColor = true;
+            this.NewButton.Visible = false;
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.AutoSize = true;
+            this.OpenButton.Location = new System.Drawing.Point(3, 32);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 27);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.OpenButton_MouseClick);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.AutoSize = true;
+            this.SaveButton.Location = new System.Drawing.Point(3, 65);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 27);
+            this.SaveButton.TabIndex = 1;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Visible = false;
+            this.SaveButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.SaveButton_MouseClick);
+            // 
+            // RunButton
+            // 
+            this.RunButton.AutoSize = true;
+            this.RunButton.Location = new System.Drawing.Point(738, 3);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(75, 27);
+            this.RunButton.TabIndex = 1;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Visible = false;
+            this.RunButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.RunButton_MouseClick);
+            // 
+            // TextEditorBox
+            // 
+            this.TextEditorBox.AcceptsTab = true;
+            this.TextEditorBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TextEditorBox.Font = new System.Drawing.Font("Times New Roman", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextEditorBox.Location = new System.Drawing.Point(105, 50);
+            this.TextEditorBox.MaxLength = 100000;
+            this.TextEditorBox.Multiline = true;
+            this.TextEditorBox.Name = "TextEditorBox";
+            this.TextEditorBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.TextEditorBox.Size = new System.Drawing.Size(627, 373);
+            this.TextEditorBox.TabIndex = 2;
+            this.TextEditorBox.WordWrap = false;
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.Location = new System.Drawing.Point(105, 0);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(627, 47);
+            this.TitleLabel.TabIndex = 3;
+            this.TitleLabel.Text = "Koelbedit Python IDE";
+            this.TitleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 477);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button NewButton;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.TextBox TextEditorBox;
+        private System.Windows.Forms.Label TitleLabel;
+
+
+        System.Windows.Forms.OpenFileDialog fileDialog = new System.Windows.Forms.OpenFileDialog();
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SimplePythonIDE
+{
+    public partial class Form1 : Form
+    {
+
+        string currentFilePath = "";
+        bool hasOpenedFileYet = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            DialogResult dr = fileDialog.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                TextEditorBox.Text = File.ReadAllText(fileDialog.FileName);
+                currentFilePath = fileDialog.FileName;
+                hasOpenedFileYet = true;
+                SaveButton.Visible = true;
+                RunButton.Visible = true;
+            }
+        }
+
+        private void SaveButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            File.WriteAllText(currentFilePath, TextEditorBox.Text);
+        }
+
+        private void RunButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            //string[] undisterbedFileLines = File.ReadAllLines(fileDialog.FileName);
+           // string[] tempFileLines = undisterbedFileLines;
+           // tempFileLines.Append<string>("print(\">>>>Application finished, you may close CMD.\")");
+           // tempFileLines.Append<string>("input()");
+            //File.WriteAllLines(currentFilePath,tempFileLines);
+            System.Diagnostics.Process.Start("CMD.exe", $"/Cpython {currentFilePath}");
+            //File.WriteAllLines(currentFilePath,undisterbedFileLines);
+        }
+    }
+}
